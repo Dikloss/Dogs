ceb25957b6f4a4412b5132d9f6febf104a422d83
Dikloss <diklos00@gmail.com>
2024-11-14 18:59:41 +0300

    Теперь каждое изображение в отдельном окнею Добавлены спинбоксы для установки размера ищбражения.

--- Dogs.py
+++ Dogs.py
@@ -5,8 +5,6 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
-from click import progressbar
-
 
 def get_dog_image():
     try:
@@ -27,10 +25,14 @@ def show_image():
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
-            img.thumbnail((300, 300))
+            img_size = (int(width_spinbox.get()), int(width_spinbox.get()))
+            img.thumbnail(img_size)
             img = ImageTk.PhotoImage(img)
-            label.config(image=img)
-            label.image = img # позволяет сохранить картинкуБ чтобы мусорщик не забрал
+            new_window = Toplevel(window)
+            new_window.title ("случайное изображение")
+            lb = ttk.Label(new_window, image=img)
+            lb.pack()
+            lb.image = img # позволяет сохранить картинкуБ чтобы мусорщик не забрал
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка {e}")
 
@@ -42,7 +44,6 @@ def prog():
     progress.start(30)
     window.after(3000,show_image)
 
-
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
@@ -52,7 +53,19 @@ label.pack(pady=10)
 
 button = ttk.Button(text="Загрузить изображение", command=prog)
 button.pack(pady=10)
+
 progress = ttk.Progressbar(mode="determinate", length=300)
 progress.pack(pady=10)
+
+width_label = ttk.Label(text="Ширина")
+width_label.pack(side="left", padx=(10,0))
+width_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+width_spinbox.pack(side="left", padx=(0,10))
+
+height_label = ttk.Label(text="Высота")
+height_label.pack(side="left", padx=(10,0))
+height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox.pack(side="left", padx=(0,10))
+
 
 window.mainloop()
